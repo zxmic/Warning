b7f59c900cffd4e1dbd645901cd3c9c9de3a6df4
zx <2537011619@qq.com>
2020-02-12 20:27:24 +0800

    Initial commit

---
+++ src/main/java/all/netty/Server.java
@@ -0,0 +1,74 @@
+package all.netty;
+
+import io.netty.bootstrap.ServerBootstrap;
+import io.netty.channel.*;
+import io.netty.channel.epoll.EpollEventLoopGroup;
+import io.netty.channel.epoll.EpollServerSocketChannel;
+import io.netty.channel.nio.NioEventLoopGroup;
+import io.netty.channel.socket.SocketChannel;
+import io.netty.channel.socket.nio.NioServerSocketChannel;
+import io.netty.handler.codec.http.HttpObjectAggregator;
+import io.netty.handler.codec.http.HttpRequestDecoder;
+import io.netty.handler.codec.http.HttpResponseEncoder;
+import io.netty.handler.stream.ChunkedWriteHandler;
+
+import java.util.concurrent.Executors;
+
+public class Server {
+    private void start() {
+        //创建BossGroup和workerGroup
+        EventLoopGroup bossGroup = new NioEventLoopGroup(1);//接收组，处理来访问服务器的客户的连接请求
+        EventLoopGroup workGroup = new NioEventLoopGroup();//工作组，实现数据的读写
+
+        try {
+            ServerBootstrap bootstrap=new ServerBootstrap();//服务端来设置通道参数的工具
+            bootstrap.group(bossGroup, workGroup)//将两个工作线程与通道绑定
+                    .channel(NioServerSocketChannel.class)//指定NIO模式
+                    .option(ChannelOption.SO_BACKLOG,128)//设置TCP缓冲区
+                    .childOption(ChannelOption.SO_KEEPALIVE,true)//设置长连接
+                    .childHandler(new ChannelInitializer<SocketChannel>() {
+                        @Override
+                        protected void initChannel(SocketChannel socketChannel) throws Exception {
+                            socketChannel.pipeline().addLast(new HttpResponseEncoder());//server端发送的是httpResponse,要进行编码
+                            socketChannel.pipeline().addLast(new HttpRequestDecoder());//server端接收的是httpRequest,要进行解码
+
+                            //等待解码后的报文头和报文体一起扔给下一层
+                            socketChannel.pipeline().addLast(new ChunkedWriteHandler());
+                            socketChannel.pipeline().addLast(new ServerHandler());
+                        }
+                    });
+            System.out.println("服务器 is ready。。。");
+            final ChannelFuture future = bootstrap.bind("127.0.0.1", 8080).sync();//绑定端口
+            //给future注册监听器，监控关心的事件
+            future.addListener(new ChannelFutureListener() {
+                public void operationComplete(ChannelFuture channelFuture) throws Exception {
+                    if(future.isSuccess()){
+                        System.out.println("监听端口 8080 成功");
+                    }else {
+                        System.out.println("监听端口 8080 失败");
+                    }
+                }
+            });
+            //阻止程序关闭
+            future.channel().closeFuture().sync();
+            System.out.println("xxx");
+
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }finally {
+            bossGroup.shutdownGracefully();
+            workGroup.shutdownGracefully();
+        }
+
+    }
+
+    public static void main(String[] args) {
+        new Thread(){
+            @Override
+            public void run() {
+                Server server=new Server();
+                server.start();
+            }
+        }.start();
+    }
+}
---
+++ src/main/java/all/netty/ServerHandler.java
@@ -0,0 +1,46 @@
+package all.netty;
+
+import io.netty.buffer.ByteBuf;
+import io.netty.buffer.Unpooled;
+import io.netty.channel.ChannelHandlerContext;
+import io.netty.channel.SimpleChannelInboundHandler;
+import io.netty.handler.codec.http.*;
+import io.netty.util.CharsetUtil;
+
+import java.nio.charset.Charset;
+
+/*
+说明：
+1、SimpleChannelInboundHandler是ChannelInboundHandlerAdapter
+2、HttpObject 客户端和服务器端相互通讯的数据被封装成HttpObject
+ */
+public class ServerHandler extends SimpleChannelInboundHandler<HttpObject> {
+    //服务器数据处理
+    @Override
+    public void channelRead(ChannelHandlerContext ctx, Object msg) throws Exception {
+
+
+
+    }
+    //channelRead0读取客户端数据
+    @Override
+    protected void channelRead0(ChannelHandlerContext ctx, HttpObject msg) throws Exception {
+        //判断msg是不是httprequest请求
+        if(msg instanceof HttpRequest){
+            System.out.println("msg类型="+msg.getClass());
+            System.out.println("客户端地址"+ctx.channel().remoteAddress());
+            //回复消息给浏览器http
+            ByteBuf context = Unpooled.copiedBuffer("hello,我是服务器！", CharsetUtil.UTF_8);
+
+            //构造一个http的回应，即httpresponse
+            FullHttpResponse response=new DefaultFullHttpResponse(HttpVersion.HTTP_1_0,HttpResponseStatus.OK,context);
+            response.headers().set(HttpHeaderNames.CONTENT_TYPE,"text/plain");
+            response.headers().set(HttpHeaderNames.CONTENT_LENGTH,context.readableBytes());
+            //将创建好的response返回
+            ctx.writeAndFlush(response);
+
+        }
+    }
+
+
+}
