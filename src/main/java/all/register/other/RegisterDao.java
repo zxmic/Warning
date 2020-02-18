6599d4f71880c0377766746725b90155d6fe00a7
zx <2537011619@qq.com>
2020-02-18 22:47:05 +0800

    zx

---
+++ src/main/java/all/register/other/RegisterDao.java
@@ -0,0 +1,90 @@
+package all.register.other;
+
+import all.login.other.User;
+import all.util.JdbcUtil;
+
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+public class RegisterDao {
+
+    Connection conn;
+    Statement stmt;
+    ResultSet rs;
+
+    public RegisterDao(){
+        conn= JdbcUtil.getConnection();
+        try {
+            stmt = conn.createStatement();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public boolean userAdd(Long userid,String phonenumber, String password){
+
+        try{
+            //添加用户信息
+            String sql = "insert into user(userid,phonenumber,password) values('"+ userid +"','"+phonenumber+"','"+password+"')";
+            stmt.execute(sql);
+        }catch (Exception e) {
+            e.printStackTrace();
+            return false;
+        }
+        return true;
+    }
+
+    public boolean userUpdatePass(String userid, String password){
+        try{
+            String sql = "update user set password= '"+password+"' WHERE userid='"+userid+"'";
+            stmt.execute(sql);
+        }catch (Exception e) {
+            e.printStackTrace();
+            return false;
+        }
+        return true;
+   }
+
+    public boolean userUpdatePhone(String userid, String phone){
+        try{
+            String sql = "update user set phonenumber= '"+phone+"' WHERE userid='"+userid+"'";
+            stmt.execute(sql);
+        }catch (Exception e) {
+            e.printStackTrace();
+            return false;
+        }
+        return true;
+    }
+
+    public User userSelect(String userid){
+
+        User user=new User();
+        String sql="select userid,phonenumber,password from user where userid ='"+userid+"'";
+        try {
+            rs=stmt.executeQuery(sql);
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        try {
+            if(rs.next()){
+                user.setUserid(rs.getInt("userid"));
+                user.setPhoneNumber(rs.getString("phonenumber"));
+                user.setPassword(rs.getString("password"));
+                return user;
+            }
+        } catch (SQLException e) {
+            System.out.println("没有搜索到user信息");
+            e.printStackTrace();
+        }
+        return null;
+    }
+
+    @Override
+    protected void finalize() throws Throwable {
+        super.finalize();
+        JdbcUtil.close_2(conn,stmt,rs);
+    }
+
+}
---
+++ src/main/java/all/register/other/RegisterService.java
@@ -0,0 +1,38 @@
+package all.register.other;
+
+
+import all.login.other.User;
+
+public class RegisterService {
+    private RegisterDao registerDao=new RegisterDao();
+
+    public boolean userAdd(Long userid,String phonenumber, String password){
+        boolean flag=false;
+        boolean isAdd=registerDao.userAdd(userid,phonenumber,password);
+        if(isAdd){
+            flag=true;
+            return flag;
+        }
+        return flag;
+    }
+
+    public boolean userUpdatePhone(String userid, String phonenumber){
+        User user=userSelect(userid);
+        boolean flag=false;
+        if(user!=null){
+            flag=registerDao.userUpdatePhone(userid,phonenumber);
+        }
+        return flag;
+
+    }
+
+    public User userSelect(String userid){
+        User user=registerDao.userSelect(userid);
+        if(user!=null){
+            return user;
+        } else{
+            return null;
+        }
+    }
+
+}
